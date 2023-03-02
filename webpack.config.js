670f30c254d740ea8971f54fe68ef8c056c9fc92
Greg Schneider <greg53x@msn.com>
2023-03-01 22:32:23 -0700

    configure webpack to import css

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,12 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
 
     },
-
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
