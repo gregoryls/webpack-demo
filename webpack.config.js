c3f4bb793da9b891d83f9ce9182d28197ef08d69
Greg Schneider <greg53x@msn.com>
2023-03-09 13:00:53 -0700

    inline source map

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,12 @@ const json5 = require('json5');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
       index: './src/index.js',
       print: './src/print.js',
     },
+    devtool: 'inline-source-map',
     output: {
         filename: '[name].bundle.js', 
         path: path.resolve(__dirname, 'dist'),
@@ -22,7 +24,7 @@ module.exports = {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        title: 'Output management',
+        title: 'Development',
         minify: false,
       }),
     ],
